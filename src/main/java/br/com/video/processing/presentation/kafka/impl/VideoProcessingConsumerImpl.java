46ddfb54afa73246c9ee869e577217e165c24e42
Carlos A M Ferreira <c.a.mascarenhas@hotmail.com>
2025-09-28 22:29:27 -0300

    fix: configure thread pool settings and add blocking annotation to video processing consumer

--- src/main/java/br/com/video/processing/presentation/kafka/impl/VideoProcessingConsumerImpl.java
+++ src/main/java/br/com/video/processing/presentation/kafka/impl/VideoProcessingConsumerImpl.java
@@ -3,6 +3,7 @@ package br.com.video.processing.presentation.kafka.impl;
 import br.com.video.processing.application.controller.VideoProcessingController;
 import br.com.video.processing.common.domain.dto.request.UploadedVideoInfoDto;
 import br.com.video.processing.presentation.kafka.VideoProcessingConsumer;
+import io.smallrye.reactive.messaging.annotations.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -12,9 +13,9 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 public class VideoProcessingConsumerImpl implements VideoProcessingConsumer {
     private final VideoProcessingController videoProcessingController;
 
+    @Blocking(ordered = false)
     @Incoming("video-split")
     public void consume(UploadedVideoInfoDto uploadedVideoInfoDto) {
         videoProcessingController.processVideo(uploadedVideoInfoDto);
     }
-
 }
